81f96307e6a64dff13073c0f9dc7de85278a9364
TheLordOfBugs <66015195+re-3v0lv3d@users.noreply.github.com>
2025-03-25 17:50:27 +0100

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,14 @@
+// constants.js
 const COLS = 10;
 const ROWS = 20;
 const BLOCK_SIZE = 30;
+
+const piezas = [
+  { forma: [[0, 0], [1, 0], [1, 1], [0, 1]], color: 'yellow' },
+  { forma: [[0, 0], [1, 0], [2, 0], [3, 0]], color: 'cyan' },
+  { forma: [[0, 0], [1, 0], [2, 0], [2, 1]], color: 'orange' },
+  { forma: [[0, 1], [1, 1], [2, 1], [2, 0]], color: 'blue' },
+  { forma: [[1, 0], [2, 0], [0, 1], [1, 1]], color: 'red' },
+  { forma: [[0, 0], [1, 0], [1, 1], [2, 1]], color: 'green' },
+  { forma: [[1, 0], [0, 1], [1, 1], [2, 1]], color: 'purple' }
+];
